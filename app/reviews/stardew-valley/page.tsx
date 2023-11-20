9254d0b8539fcc4d72788b933a83257fa02f2733
Benoit Havet <bhavet_ext@christiandior.com>
2023-11-20 15:44:40 +0100

    use front gray-matter

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import Heading from "@/components/Heading";
 import { readFile } from "node:fs/promises";
 import { marked } from "marked";
+import matter from "gray-matter";
 
 export default async function StardewValleyPage() {
   const text = await readFile('./content/reviews/stardew-valley.md', 'utf8');
-  const html = marked(text)
+  const { content, data: { title, image, date} } = matter(text) 
+  const html = marked(content);
+  
     return (
         <>
           <Heading>
-          Stardew Valley
+          {title} 
           </Heading>
-          <img src="/images/stardew-valley.jpg" alt="" width={600} height={360} className="mb-2 rounded" />
+          <p className="italic pb-2">{date}</p>
+          <img src={image} alt="" width={600} height={360} className="mb-2 rounded" />
           <article dangerouslySetInnerHTML={{ __html: html}}
            className="max-w-screen-sm prose prose-slate"
           />
