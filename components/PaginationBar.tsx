ca0e2f903d4555eedbd17c1a5200a5726c12db48
Benoit Havet <bhavet_ext@christiandior.com>
2023-11-28 12:09:36 +0100

    pagination Bar component

--- components/PaginationBar.tsx
+++ components/PaginationBar.tsx
@@ -11,10 +11,12 @@ export default function PaginationBar({ page, pageCount}:P) {
    <div className="flex gap-2 pb-3 items-center">
     <PaginationLink href={`/reviews?page=${page - 1}`} enabled={page > 1}>
       <ChevronLeftIcon className="h-5 w-5"/>
+      <span className="sr-only">Previous page</span>
     </PaginationLink>
      <span>Page {page} of {pageCount}</span>
      <PaginationLink href={`/reviews?page=${page + 1}`} enabled={page < pageCount}>
       <ChevronRightIcon className="h-5 w-5"/>
+      <span className="sr-only">Next page</span>
     </PaginationLink>
    </div>
  )
@@ -24,8 +26,7 @@ function PaginationLink({ children, href, enabled }) {
     if(!enabled) {
         return (
            <span  
-              area-aria-disabled 
-              className="border flex gap-1 rounded text-slate-300 text-sm hover:bg-orange-50 hover:cursor-pointer"
+              className="border flex gap-1 rounded text-slate-300 text-sm hover:bg-orange-50 hover:cursor-not-allowed"
             >
             {children}
            </span> 
